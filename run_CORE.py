3684e1f369a84296ae8dbe51f8fa7d4bfade038e
evan <evanyl2@ics.uci.edu>
2026-05-17 21:57:49 -0700

    add graph and modify core input

--- run_CORE.py
+++ run_CORE.py
@@ -1,34 +1,46 @@
-from CORE import CORE  # type: ignore
+from CORE import CORE 
+from load_data import remap_to_dense
+import time
+
+
+def load_yeast_graph(path):
+    adj = {}
+
+    with open(path) as f:
+        for line in f:
+            if not line.strip():
+                continue
+
+            u, v = line.strip().split(';')
+
+            adj.setdefault(u, set()).add(v)
+            adj.setdefault(v, set()).add(u)
+
+    return adj
+
 
 if __name__ == "__main__":
-    from load_data import read_csv_graph, read_clq_graph, remap_to_dense
-    import time
-
     # load graph
-    adj_dict = read_csv_graph('graphs/challenge.csv.gz')
-    #dj_dict = read_clq_graph('graphs/brock400_2.clq')
-    #adj_dict = read_clq_graph('graphs/brock800_2.clq')
+    adj_dict = load_yeast_graph('graphs/yeast.csv')
 
     adj_dict, id_map, reverse_map = remap_to_dense(adj_dict)
-    
-    # run search 
-    total_start_time = time.time()
+
+    start_time = time.time()
     best_clique, best_time = CORE(
-        adj_dict,                      # adjacency dictionary representing the graph
-        gamma = 0.25,                  # density threshold for quasi-clique
-        no_improve_limit = 100,        # max iterations without improvement before stopping current k-size search
-        max_iterations = 1e5,          # max global iterations across all k-sizes
-        region_freq = 10,              # trigger region exploration every N iterations of no improvement
-        min_region_size = 10,          # minimum number of nodes to include in region
-        max_region_size = 200,         # maximum number of nodes to include in region
-        seed = 123,                    # random seed 
+        adj_dict,
+        gamma=0.25,
+        no_improve_limit=100,
+        max_iterations=1e5,
+        region_freq=10,
+        min_region_size=10,
+        max_region_size=200,
+        seed=123,
         fixed_k=369
     )
-    total_time = time.time() - total_start_time
 
-    # get solution with original ID's   
+    total_time = time.time() - start_time
+
     original_clique = [reverse_map[node] for node in best_clique]
 
-    # Print info 
     print(f"\nFound solution with {len(original_clique)} vertices in {best_time:.4f} seconds")
     print(f"Total time: {total_time:.4f} seconds")
